2084d52a6fefab59bcb42b98ab5524d9d2e9eb16
aiya000 <aiya000.develop@gmail.com>
2022-05-28 17:46:24 +0900

    :sparkles:

--- Record-Replay.ps1
+++ Record-Replay.ps1
@@ -2,16 +2,20 @@ Param([String]$title = "VRChat")
 
 # Configurations
 $keysToRecord = "^+%p"
-$shotSound = $Home + '\Desktop\bin\shot.wav'
-$intervalToLoadApp = 400  # NOTE: This may depend your machine performance. Please adjust this.
+$intervalToLoadApp = 1000 # NOTE: This may depend your machine performance. Please adjust this.
 
+Write-Output 'Start'
+
+#
 # Record replay
+#
 Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName Microsoft.VisualBasic
+
 $process = Get-Process -Name $title
-(New-Object -ComObject WScript.Shell).AppActivate($process.MainWindowTitle)
+[Microsoft.VisualBasic.Interaction]::AppActivate($process.ID)
+# (New-Object -ComObject WScript.Shell).AppActivate($process.MainWindowTitle)
 Start-Sleep -m $intervalToLoadApp
 [System.Windows.Forms.SendKeys]::SendWait($keysToRecord)
 
-# Notify success by the sound
-$player = New-Object Media.SoundPlayer($shotSound)
-$player.PlaySync()
+Write-Output 'Done!'
